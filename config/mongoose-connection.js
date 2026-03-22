08abe3dfd2040f4eb390b0caa82e5534dce30fe9
Diya Jain <diyaj5330@gmail.com>
2026-03-22 11:27:45 +0530

    fixed mongodb connection using env

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,17 +1,7 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
-const config = require("config");
 
-mongoose
-    .connect(`${config.get("MONGODB_URI")}/scatch`)
-    .then(function () {
-        dbgr("connected");
-    })
-    .catch(function (err) {
-        dbgr(err);
-    });
-
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(`${process.env.MONGODB_URI}/scatch`)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
