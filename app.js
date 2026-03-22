ef2b5aa205f30c515efc67584cff064d1e0ad26c
Diya Jain <diyaj5330@gmail.com>
2026-03-23 01:08:17 +0530

    fix: corrected server port / deployment issue

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 
 // Server
-require('dotenv').config();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
