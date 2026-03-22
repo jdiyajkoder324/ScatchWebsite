7ba258c0ca0ec2436bf78c8d83350063218c59b3
Diya Jain <diyaj5330@gmail.com>
2026-03-23 01:55:18 +0530

    fix: added login route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -10,9 +10,18 @@ router.get("/profile", (req, res) => {
     res.send("User Profile Page");
 });
 
+router.get("/login", (req, res) => {
+  res.send("Login page working");
+});
+
 router.post("/register", (req, res) => {
     console.log(req.body);
     res.send("Register page working");
+});
+
+router.post("/login", (req, res) => {
+  console.log("Login hit ✅", req.body);
+  res.send("User logged in");
 });
 
 // MUST export router directly
