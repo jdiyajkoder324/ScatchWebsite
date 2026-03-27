95f97b91a3ef7a8f9e07ef6965d939d39afa3397
Diya Jain <diyaj5330@gmail.com>
2026-03-27 09:54:06 +0530

    Fixed MongoDB connection reverted to local

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 
-mongoose.connect(`${process.env.MONGODB_URI}/scatch`)
+mongoose.connect("mongodb://127.0.0.1:27017/scatch")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
